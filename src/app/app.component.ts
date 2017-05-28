1b153689655901853e2db9c1d098a5a091279b46
nirkaufman <nirkaufman@gmail.com>
2017-05-28 23:06:03 +0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>Dynamic Forms</h1>
+  `,
 })
 export class AppComponent {
-  title = 'app works!';
+
 }
